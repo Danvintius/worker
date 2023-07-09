d23585dfcb7bd83233e83079ac96d54cebc4f6e0
Danvintius <danila.94@mail.ru>
2023-07-09 22:16:03 +0300

    commit

---
+++ src/Worker.java
@@ -0,0 +1,22 @@
+public class Worker {
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback) {
+        this.callback = callback;
+    //    this.errorCallback = errorCallback;
+    }
+
+    public Worker(OnTaskErrorListener errorCallback) {
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+            if (i == 33) {
+                errorCallback.onError();
+            }
+        }
+    }
+}
